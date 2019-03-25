4cdbc02ae11e47f80c725dad237a2604b3c98436
P00HP00H <32670787+P00HP00H@users.noreply.github.com>
2019-03-25 17:30:00 +0900

    Update 5430.py

--- 5430.py
+++ 5430.py
@@ -1,3 +1,4 @@
+// 풀이 1
 a = int(input())	# Case 갯수
 
 for i in range(0, a):
@@ -33,6 +34,55 @@ for i in range(0, a):
             print("[", end = "")
             for m in range(0, len(e)):      # 리스트를 그냥 출력하면 요소가 모두 int형이라고 해도 [1, 2, 3] 이렇게 된다. 하지만 [1,2,3] 이런 식으로 출력해야 하기 때문에 이렇게 해준 것이다.
                 if m == len(e) - 1:         # 현재 ['1', '2', '3'] 이런 식으로 되어 있기 때문에 먼저 문자'['를 출력하고, 각 요소들에 int()를 붙여서 int형으로 만든 후 ','를 붙여준다. 마지막 요소에는 ']'를 붙여준다.
+                    print(int(e[m]), end = "]")
+                    print("")
+                else:
+                    print(int(e[m]), end = ",")
+                    
+                    
+                    
+                    
+                    
+// 풀이 2
+
+import sys
+a = int(sys.stdin.readline())
+ 
+for i in range(0, a):
+    b = sys.stdin.readline()
+    c = int(sys.stdin.readline())
+    d = sys.stdin.readline()[1:-2]
+ 
+    if len(d) == 0:
+        e = []
+    elif len(d) == 1:
+        e = [d]
+    elif len(d) > 1:
+        e = d.split(',')
+ 
+    num = 0
+    for k in range(0, len(b)):
+        if b[k] == "D":
+            if len(e) == 0:
+                print('Error')
+                num += 1
+                break
+            else:
+                e.pop(0)
+        elif b[k] == "R":
+            reverse = []        # 순서 바꾼 리스트
+            for l in range(0, len(e)):
+                reverse.append(e[-1])
+                e.pop(-1)
+            e = reverse         # e 변수에 reverse 선언
+ 
+    if num == 0:
+        if len(e) == 0:
+            print(e)
+        else:
+            print("[", end = "")
+            for m in range(0, len(e)): 
+                if m == len(e) - 1:  
                     print(int(e[m]), end = "]")
                     print("")
                 else:
